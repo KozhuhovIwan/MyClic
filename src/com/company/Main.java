385a8f4db799d9192a0cc86821dd08eb574ea510
KozhuhovIwan <iv.kozhuhov@yandex.ru>
2021-12-23 19:13:14 +0300

    one Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World?");
+        int i = 2;
+        int a = 2;
+        System.out.println(2+2);
+        System.out.println(2 + "2");
+        System.out.println(i+a*i);
+
     }
 }
